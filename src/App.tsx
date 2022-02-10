1f1b2e8cf58bf2bd30197b071d78937944bea4e3
Guilherme Alcântara <gui.absilva@outlook.com>
2022-02-10 17:05:10 -0300

    2º Dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-function App() {
+import { createContext, useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { auth, firebase } from "./services/firebase";
+
+import {AuthContextProvider} from './contexts/AuthContext'
+
+
+function App() {
   return (
-    <div className="App">
-   <h1>Hello</h1>
-    </div>
-  )
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </AuthContextProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-import './services/firebase'
+import "./services/firebase";
+
+import "./styles/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
